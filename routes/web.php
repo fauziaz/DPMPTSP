8e379156b31c4a880b988b10900b8dcc26eceed8
fauziaazahra1@gmail.com <fauzia.depriani@mhs.unsoed.ac.id>
2025-08-06 09:31:49 +0700

    routes

--- routes/web.php
+++ routes/web.php
@@ -44,16 +44,3 @@ Route::post('/berita/{id}/share', function ($id) {
 Route::get('/berita/create', Create::class)->name('berita.create');
 
 
-Route::view('dashboard', 'dashboard')
-    ->middleware(['auth', 'verified'])
-    ->name('dashboard');
-
-Route::middleware(['auth'])->group(function () {
-    Route::redirect('settings', 'settings/profile');
-
-    Volt::route('settings/profile', 'settings.profile')->name('settings.profile');
-    Volt::route('settings/password', 'settings.password')->name('settings.password');
-    Volt::route('settings/appearance', 'settings.appearance')->name('settings.appearance');
-});
-
-require __DIR__.'/auth.php';
